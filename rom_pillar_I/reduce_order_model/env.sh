5ce211ea15715cbfd1898b6763092b68a235ed93
Jorge Ejarque <jorge.ejarque@bsc.es>
2023-10-17 14:03:10 +0200

    fixes in pillar I workflow

--- rom_pillar_I/reduce_order_model/env.sh
+++ rom_pillar_I/reduce_order_model/env.sh
@@ -1,6 +1,6 @@
 . /etc/profile.d/z10_spack_environment.sh
 export COMPSS_HOME=/opt/view/compss
-export PYTHONPATH=/opt/view/:$PWD:/opt/view/lib/python3/dist-packages/:$PYTHONPATH
+export PYTHONPATH=/opt/view/:/reduce_order_model/src:/opt/view/lib/python3/dist-packages/:$PYTHONPATH
 export LD_LIBRARY_PATH=$COMPSS_HOME/Bindings/bindings-common/lib/:/opt/view/libs:$LD_LIBRARY_PATH
 export ComputingUnits=8
 export KRATOS_CUS=8
