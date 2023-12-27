5c187f2b61b1711f637e29b08385f96aad027948
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2023-12-27 10:18:00 +0100

    Fix indentation, re-format code (4 spaces, pep8)

--- Pillar_II/esm/awicm3/src/hecuba_lib/fesom_datamodel.py
+++ Pillar_II/esm/awicm3/src/hecuba_lib/fesom_datamodel.py
@@ -1,6 +1,7 @@
 from hecuba import StorageDict
 
-class fesom_outputs (StorageDict):
-   '''
-   @TypeSpec dict <<lat:float,ts:int>,metrics:numpy.ndarray>
-   '''
+
+class fesom_outputs(StorageDict):
+    '''
+    @TypeSpec dict <<lat:float,ts:int>,metrics:numpy.ndarray>
+    '''
