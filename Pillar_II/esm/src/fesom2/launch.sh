76c6eae158b8cb602ed03b8ece00de098ce69826
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2023-12-28 18:27:38 +0100

    Move Hecuba configuration to same level as other config, shellcheck launch_mn4.sh now renamed/updated as launch.sh

--- Pillar_II/esm/src/fesom2/launch.sh
+++ Pillar_II/esm/src/fesom2/launch.sh
@@ -7,15 +7,18 @@ set -o errexit -o pipefail -o noclobber -o nounset
 
 # This script is used to launch experiments with FESOM2.
 
+MODEL="fesom2"
+
 HPC=""
 DEBUG=""
 CORES=0
+CORES_PER_NODE=48
 MEMBERS=1
 QOS=debug
 
 # Parse options. Note that options may be followed by one colon to indicate
 # they have a required argument.
-if ! options=$(getopt --name "$(basename "$0")" --options dc:m:q: --longoptions debug,cores:,members:,qos:,hpc: -- "$@"); then
+if ! options=$(getopt --name "$(basename "$0")" --options dc:m:q: --longoptions debug,cores:,members:,qos:,hpc:,cores_per_node: -- "$@"); then
   # Error, getopt will put out a message for us
   exit 1
 fi
@@ -45,6 +48,10 @@ while [ $# -gt 0 ]; do
     HPC="${2,,}"
     shift
     ;;
+  --cores_per_node)
+    CORES_PER_NODE="${2}"
+    shift
+    ;;
   --)
     shift
     break
@@ -71,33 +78,49 @@ if [ "${CORES}" -le 0 ]; then
   exit 1
 fi
 
+if [ "${CORES_PER_NODE}" -le 0 ]; then
+  echo "Cores per node must be equal or greater than 1"
+  exit 1
+fi
+
 if [ "${MEMBERS}" -le 0 ]; then
   echo "Members must be equal or greater than 1"
   exit 1
 fi
 
+printf "Launching %s eFlows4HPC ESM experiment...\U1F680\n" "${MODEL}"
+
 # Hecuba configuration
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-HECUBA_CONFIGURATION="$(realpath -e -- "${SCRIPT_DIR}/../hecuba_lib/storage_props.cfg")"
+HECUBA_CONFIGURATION="$(realpath -e -- "${SCRIPT_DIR}/storage_props.cfg")"
 
 # Experiment configuration. The variables exported here are used
 # by PyCOMPSs (some Python decorators use values like ="${FESOM_CORES}").
-export FESOM_CORES=${CORES}
-NODES_PER_CORE=48
+FESOM_CORES=${CORES}
+
+# math.ceil(fesom_cores / cores_per_node)
+NODE_ALLOCATION="$(((FESOM_CORES + CORES_PER_NODE - 1) / CORES_PER_NODE))"
+# Now multiply by number of ensemble members...
+NODE_ALLOCATION="$((NODE_ALLOCATION * MEMBERS))"
+
+if [ "${CORES}" -lt "${CORES_PER_NODE}" ]; then
+  echo "WARNING: You have ${CORES_PER_NODE} cores per node, but requested less: ${CORES}"
+fi
+if [ $((CORES % CORES_PER_NODE)) -ne 0 ]; then
+  echo "WARNING: You are not using all the cores of your nodes (${CORES_PER_NODE}), you requested: ${CORES}"
+fi
+
+export FESOM_CORES
 export QOS
 export MEMBERS
-NODE_ALLOCATION="$(((FESOM_CORES / NODES_PER_CORE) * MEMBERS))"
-if [ ${NODE_ALLOCATION} -eq 0 ]; then
-  NODE_ALLOCATION=1
-fi
 export NODE_ALLOCATION
 
 echo -e "\nLaunch arguments:\n"
 
-echo "MODEL           : FESOM2"
+echo "MODEL           : ${MODEL}"
 echo "HPC             : ${HPC}"
 echo "CORES           : ${FESOM_CORES}"
-echo "NODES PER CORE  : ${NODES_PER_CORE}"
+echo "CORES PER NODE  : ${CORES_PER_NODE}"
 echo "NODES           : ${NODE_ALLOCATION}"
 echo "QOS             : ${QOS}"
 echo "MEMBERS         : ${MEMBERS}"
@@ -108,7 +131,7 @@ HPC_ENV_FILE="$(realpath -e -- "${SCRIPT_DIR}/env/${HPC}.sh")"
 # this file (you can lint all files independently from this).
 # shellcheck source=env/mn4.sh
 source "${HPC_ENV_FILE}"
-echo -e "Done! ${HPC^^} environment loaded correctly!\n"
+echo -e "Done! ${HPC} environment loaded correctly!\n"
 
 # Sample invocation of this script:
 #
@@ -127,8 +150,8 @@ enqueue_compss -t -g -d --sc_cfg=mn.cfg \
   --exec_time=120 \
   --keep_workingdir \
   --worker_working_dir="${PWD}" \
-  --worker_in_master_cpus=48 \
+  --worker_in_master_cpus="${CORES_PER_NODE}" \
   --num_nodes="${NODE_ALLOCATION}" \
   --pythonpath="${PWD}":"${HECUBA_ROOT}/compss" esm_simulation.py \
-  --model fesom2 \
+  --model "${MODEL}" \
   "${DEBUG}"
