274ee776e73562100a771c9b382c6f0c7f089689
Rohan Ahmed <rohan.ahmed7170@gmail.com>
2024-02-15 10:22:40 +0100

    make dummy pruning a pycompss task

--- Pillar_II/esm/src/dummy_prune_analysis.py
+++ Pillar_II/esm/src/dummy_prune_analysis.py
@@ -1,3 +1,12 @@
+from pycompss.api.api import TaskGroup  # type: ignore
+from pycompss.api.api import compss_barrier_group  # type: ignore
+from pycompss.api.api import compss_cancel_group  # type: ignore
+from pycompss.api.api import compss_wait_on  # type: ignore
+from pycompss.api.exceptions import COMPSsException  # type: ignore
+from pycompss.api.on_failure import on_failure  # type: ignore
+from pycompss.api.parameter import IN  # type: ignore
+from pycompss.api.task import task  # type: ignore
+from pycompss.runtime.management.classes import Future  # type: ignore
 from hecuba import StorageDict
 import time
 import random
@@ -14,6 +23,7 @@ def simulation_started(expid):
     else:
         return False
 
+@task(expid=IN)
 def esm_analysis_prune(expid):
     
     while True:
@@ -23,10 +33,11 @@ def esm_analysis_prune(expid):
         print("Simulation {expid} not found sleeping +5 seconds")
         time.sleep(5)
     
-        mdc = MetaDictClass.get_by_alias(expid)
-        prune_sec = random.randint(10,20) 
-        time.sleep(prune_sec) 
-        mdc['prune'] = "true"
-        return f"Pruned {expid} after {prune_sec} seconds"
+    mdc = MetaDictClass.get_by_alias(expid)
+    prune_sec = random.randint(10,20) 
+    time.sleep(prune_sec) 
+    mdc['prune'] = "true"
+    return f"Pruned {expid} after {prune_sec} seconds"
+
 
 # esm_analysis_prune(expid) has to be called by pycompss as analysis         
