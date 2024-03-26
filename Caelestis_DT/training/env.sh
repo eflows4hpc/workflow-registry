e8ee9f611bb5ccdae693f4a2da5cfb10c2390052
Jorge Ejarque <jorgee@users.noreply.github.com>
2024-03-26 10:17:35 +0100

    Update env.sh

--- Caelestis_DT/training/env.sh
+++ Caelestis_DT/training/env.sh
@@ -3,13 +3,11 @@ old_PYTHONPATH=$PYTHONPATH
 export ALYA_BIN=/opt/view/bin/alya
 export ALYA_PROCS=96
 export ALYA_PPN=48
-#Así distribuye en 4 nodos
 export ComputingUnits=1
 export PYTHONPATH=$PYTHONPATH:$old_PYTHONPATH:/usr/local/lib/python3.8/dist-packages/:/usr/lib/python3/dist-packages/
 export COMPSS_HOME=/opt/view/compss
 export LD_LIBRARY_PATH=$COMPSS_HOME/Bindings/bindings-common/lib/:$LD_LIBRARY_PATH
-#export COMPSS_MPI_TYPE=ompi
 export COMPSS_MPI_TYPE=ompi
-export MPI_RUNNER_SCRIPT=$PWD/mpi_run.sh
-export SINGULARITYENV_MPI_RUNNER_SCRIPT=$MPI_RUNNER_SCRIPT
+#export MPI_RUNNER_SCRIPT=$PWD/mpi_run.sh
+#export SINGULARITYENV_MPI_RUNNER_SCRIPT=$MPI_RUNNER_SCRIPT
 export PATH=/opt/view/bin/:$PATH
